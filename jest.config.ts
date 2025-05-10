bb0c5dc6cdb54e003bf72e8d8971334de9a04005
James Penera <jamespenera@icloud.com>
2025-05-10 18:43:40 +0400

    unit test jest package installed

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom', 
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+};
+
+export default createJestConfig(customJestConfig);
